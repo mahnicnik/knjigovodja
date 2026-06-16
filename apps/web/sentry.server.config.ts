b94a2fb63cb4498d1f034c8c9b0a8151b80c863e
Nik Mahnic <mahnic.nik@gmail.com>
2026-06-16 17:20:58 +0200

    feat: Sentry error monitoring

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,13 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://d6e25ae8b7aafffcfe12de2f41273e58@o4511575751917568.ingest.de.sentry.io/4511575763779664",
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
